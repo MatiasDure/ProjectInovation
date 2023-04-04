9c39ded8cbf2348fd46e852d82a6060bf0ac0961
Matias Dure <497232@student.saxion.nl>
2023-04-04 16:52:59 +0200

    finish commit

--- Assets/QRCode/Script/QrCodeGenerator.cs
+++ Assets/QRCode/Script/QrCodeGenerator.cs
@@ -10,6 +10,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using ZXing;
 using ZXing.QrCode;
+using System.Net.NetworkInformation;
 
 public class QrCodeGenerator : MonoBehaviour
 {
@@ -27,34 +28,79 @@ public class QrCodeGenerator : MonoBehaviour
         _encodedText = new Texture2D(256, 256);
 
         //get ip address
-        Process process = new Process();
-        process.StartInfo.FileName = "ipconfig";
-        process.StartInfo.Arguments = "/all";
-        process.StartInfo.UseShellExecute = false;
-        process.StartInfo.RedirectStandardOutput = true;
+        //Process process = new Process();
+        //process.StartInfo.FileName = "ipconfig";
+        //process.StartInfo.Arguments = "/all";
+        //process.StartInfo.UseShellExecute = false;
+        //process.StartInfo.RedirectStandardOutput = true;
 
-        process.Start();
+        //process.Start();
 
-        string output = process.StandardOutput.ReadToEnd();
+        //string output = process.StandardOutput.ReadToEnd();
 
-        process.WaitForExit();
+        //process.WaitForExit();
 
-        Regex regex = new Regex(@"IPv4 Address[.\s\S]*?:\s*(?<ipAddress>\d+\.\d+\.\d+\.\d+)");
-        Match match = regex.Match(output);
+        //Regex regex = new Regex(@"IPv4 Address[.\s\S]*?:\s*(?<ipAddress>\d+\.\d+\.\d+\.\d+)");
+        //Match match = regex.Match(output);
 
-        if (match.Success)
+        //if (match.Success)
+        //{
+        //    // Parse the IPv4 address
+        //    string ipAddressString = match.Groups["ipAddress"].Value;
+        //    IPAddress ipAddress = IPAddress.Parse(ipAddressString);
+
+        //    // Determine the address family (IPv4 or IPv6)
+        //    if (ipAddress.AddressFamily == AddressFamily.InterNetwork)
+        //    {
+        //        string url = "http://" + ipAddressString + ":"+portNumber+ "/";
+        //        EncodeTextToQrCode(url);
+        //    }
+        //}
+
+        //tryout ------------------
+        List<IPAddress> ipv4Addresses = new List<IPAddress>();
+
+        // Get all network interfaces
+        NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+        // Iterate over each network interface
+        foreach (NetworkInterface networkInterface in interfaces)
         {
-            // Parse the IPv4 address
-            string ipAddressString = match.Groups["ipAddress"].Value;
-            IPAddress ipAddress = IPAddress.Parse(ipAddressString);
-
-            // Determine the address family (IPv4 or IPv6)
-            if (ipAddress.AddressFamily == AddressFamily.InterNetwork)
+            // Filter out loopback and tunnel interfaces
+            if (networkInterface.NetworkInterfaceType == NetworkInterfaceType.Loopback ||
+                networkInterface.NetworkInterfaceType == NetworkInterfaceType.Tunnel)
             {
-                string url = "http://" + ipAddressString + ":"+portNumber+ "/";
-                EncodeTextToQrCode(url);
+                continue;
+            }
+
+            // Filter out deactivated interfaces
+            if (networkInterface.OperationalStatus != OperationalStatus.Up)
+            {
+                continue;
+            }
+
+            // Get all IP addresses for this interface
+            IPInterfaceProperties properties = networkInterface.GetIPProperties();
+            foreach (IPAddress ipAddress in properties.UnicastAddresses)
+            {
+                // Filter out non-IPv4 addresses
+                if (ipAddress.AddressFamily != System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    continue;
+                }
+
+                // Add the IPv4 address to the list
+                ipv4Addresses.Add(ipAddress);
             }
         }
+
+        // Print out the list of IPv4 addresses
+        foreach (IPAddress ipv4Address in ipv4Addresses)
+        {
+            Console.WriteLine(ipv4Address);
+        }
+    }
+}
     }
 
     private Color32[] Encode(string textToEncode, int width, int height)
