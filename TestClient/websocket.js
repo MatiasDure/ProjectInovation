015e2b43bed38182a3a10ed64a491ba026f1e162
Matias Dure <497232@student.saxion.nl>
2023-04-13 19:02:24 +0200

    vibration + you died text + clamp arrows

--- TestClient/websocket.js
+++ TestClient/websocket.js
@@ -12,7 +12,7 @@ var amountHealth = 3;
 var origin = window.location.origin;
 var words = origin.split(':'); // typically: words[0]= "http", words[1] = something like "//192.168.0.1", words[2] = "8000" (the http server port)	
 var wsUri = "ws:"+words[1];    
-var wsPortInlcusion = wsUri+':4444/';
+var wsPortInlcusion = wsUri+':4842/';
 var websocket = new WebSocket(wsPortInlcusion);
 
 
@@ -72,6 +72,9 @@ let arr = e.data.split(":");
     {
         const onPlayerWon = new CustomEvent("playerWon", { detail: { playerName: arr[1] }});
         document.dispatchEvent(onPlayerWon);
+        if ('vibrate' in navigator) {
+            navigator.vibrate(1000);
+        }
     }
     else if(arr[0] === "sc")
     {
@@ -84,6 +87,7 @@ let arr = e.data.split(":");
                 return;
             }
         });
+        
     }
     else if(arr[0] === "csr")
     {
@@ -93,7 +97,11 @@ let arr = e.data.split(":");
     }
     else if(arr[0] === "lh") //lost heart
     {
+        if ('vibrate' in navigator) {
+            navigator.vibrate(600);
+        }
         amountHealth -= 1;
+        if(amountHealth === 0) textToWrite = "YOU ARE DEAD";
         ClearCanvas();
     }
 };
@@ -131,6 +139,8 @@ heartImg.src = "imgs/heart.png";
 
 var bgImg = new Image();
 bgImg.src = "imgs/blackboard.png";
+
+var textToWrite = "SWIPE TO MOVE";
 
 var isImgLoaded = false;
 
@@ -174,7 +184,7 @@ if(canvas.getContext)
 } 
 
 
-function DrawText(text = "SWIPE TO MOVE", font, color, posX, posY)
+function DrawText(text, font, color, posX, posY)
 {
     if(ctx === null) return;
 
@@ -212,6 +222,7 @@ function DrawMultipleImgs(imgToDraw, posX, posY, width, height, drawAmount, offs
 function ReverseArrow(ctx, fromx, fromy, tox, toy, arrowWidth, color){
     //variables to be used when creating the arrow
     var headlen = 32;
+
     var angle = Math.atan2(toy-fromy,tox-fromx);
  
     //clear canvas
@@ -258,7 +269,7 @@ function ClearCanvas()
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     DrawSingleImg(bgImg,0,0,canvas.width,canvas.height);
     drawImage();
-    DrawText("SWIPE TO MOVE", "72px fishermanBold", "white", canvas.width / 2 - 13 * 18, 180);
+    DrawText(textToWrite, "72px fishermanBold", "white", canvas.width / 2 - 13 * 18, 180);
     //DrawText("HEALTH", "48px fishermanBold", "red", canvas.width / 2, canvas.height - 48);
     DrawMultipleImgs(heartImg, canvas.width / 2, canvas.height - 150, 128, 128, amountHealth, true, 128);
 
@@ -276,21 +287,38 @@ document.addEventListener("touchmove", e => {
     const centerY = touch.pageY;
 
     let diffVec = SubVector(startPos,[centerX,centerY]);
-    
-    if(VectorLength(diffVec) > maxLength)
-    {
-        let unitVec = UnitVector(diffVec);
-        let scaledVec = ScaleVector(unitVec, maxLength);
+    let vecLen = VectorLength(diffVec);
 
-        let newEndVec = AddVector(startPos,scaledVec);
+    let unitVec;
+    let scaledVec;
+    let newEndVec;
+
+    let notTooLong = true;
+    if(vecLen > 700)
+    {
+        unitVec = UnitVector(diffVec);
+        scaledVec = ScaleVector(unitVec, 700);
+
+        newEndVec = AddVector(startPos,scaledVec);
         
         ReverseArrow(ctx,startPos[0],startPos[1],newEndVec[0],newEndVec[1],25,"red"); 
+        notTooLong = false;
+    }
 
+    if(vecLen > maxLength)
+    {
+        
+        scaledVec = ScaleVector(unitVec, maxLength);
+        newEndVec = AddVector(startPos,scaledVec);
+        
         lastValidEndPos = newEndVec;
         return;
     } 
 
-    ReverseArrow(ctx,startPos[0],startPos[1],centerX,centerY,25,"red");  
+    if(notTooLong)
+    {
+        ReverseArrow(ctx,startPos[0],startPos[1],centerX,centerY,25,"red");  
+    }
 
     lastValidEndPos[0] = centerX;
     lastValidEndPos[1] = centerY;
@@ -380,168 +408,4 @@ function VectorLength(_startPos, _endPos){
 function VectorLength(_vec){
     //Length Calculator
     return Math.sqrt(square(_vec[0]) + square(_vec[1]));
-}               
-
-//whole default script-------------------------
-// var selfId = -1;
-// var origin = window.location.origin;
-// h2 = document.querySelector("h2");//.innerHTML = origin;
-// var words = origin.split(':'); // typically: words[0]= "http", words[1] = something like "//192.168.0.1", words[2] = "8000" (the http server port)	
-// var wsUri = "ws:"+words[1]+"/";    
-
-// // http://www.websocket.org/echo.html
-
-// var button = document.querySelector("button"),
-// output = document.querySelector("#output"),
-// textarea = document.querySelector("textarea"),
-// //wsUri = "ws://127.0.0.1/",    // works
-// //wsUri = "ws://192.168.2.8/",  // This works if this is the LAN address of the web socket server
-// websocket = new WebSocket(wsUri);
-
-// button.addEventListener("click", onClickButton);
-
-// websocket.onopen = function (e) {
-//     writeToScreen("CONNECTED");
-// };
-
-// websocket.onclose = function (e) {
-//     writeToScreen("DISCONNECTED");
-// };
-
-// websocket.onmessage = function (e) {
-//     writeToScreen("<span>RESPONSE: " + e.data + "</span>");
-//     let arr = e.data.split(":");
-//     if(arr[0] === "ja" && 
-//     selfId < 0)
-//     {
-//         selfId = arr[1];
-//         writeToScreen("Your id is: "+selfId);
-//     }      
-// };
-
-// websocket.onerror = function (e) {
-//     writeToScreen("<span class=error>ERROR:</span> " + e.data);
-// };
-
-// var lastMessageSent = 0;
-// function doSend(message) {
-//     //writeToScreen("SENT: " + message);
-//     websocket.send(message);
-//     lastMessageSent = Date.now();
-// }
-
-// setInterval(function() {
-//     if (Date.now() - lastMessageSent >= 30) {
-//         websocket.send("filler data");
-//     }
-// }, 10); 
-
-// function writeToScreen(message) {
-//     output.insertAdjacentHTML("afterbegin", "<p>" + message + "</p>");
-// }
-
-// function onClickButton() {
-//     var text = textarea.value;
-    
-//     text && doSend(text);
-//     textarea.value = "";
-//     textarea.focus();
-// }
-
-// writeToScreen("Websocket address: "+wsUri);
-
-// //dot code -----------------------------------------------------------------
-// const square = (number) => number ** 2;
-// var startPos = [ , ];   //start point
-// var endPos = [ , ];     //end point
-// var vecLength;          //vector Length of start and end vector
-// var vectorString = "";
-
-// //On Touching the screen
-// document.addEventListener("touchstart", e => {
-    
-//     [...e.changedTouches].forEach(touch => {        //e.changedTouches is technically a list--> it does not have arry functions --> need to convert it to array first [..]
-//         const dot = document.createElement("div")
-//         dot.classList.add("dot");                    //create a class and assign dot to dot class
-        
-//         // Store vector 
-//         startPos[0] = touch.pageX;
-//         startPos[1] = touch.pageY;
-        
-//         //Set the dot pos to center of touchpoint (for showing)
-//         const dotWidth = dot.offsetWidth;
-//         const dotHeight = dot.offsetHeight;
-//         const centerX = touch.pageX - (dotWidth / 2);
-//         const centerY = touch.pageY - (dotHeight / 2);
-//         dot.style.top = `${centerY}px`;
-//         dot.style.left = `${centerX}px`;
-        
-//         dot.id = touch.identifier;           //each touch receives an id
-//         document.body.append(dot);           //append to see
-        
-//     })
-// })           
-
-// //On Touch moving
-// document.addEventListener("touchmove", e => {
-//     [...e.changedTouches].forEach(touch =>{     //for each touch
-//         const dot = document.getElementById(touch.identifier);
-        
-//         //set dot position to center of touchpoint
-//         const dotWidth = dot.offsetWidth;
-//         const dotHeight = dot.offsetHeight;
-//         const centerX = touch.pageX - (dotWidth / 2);
-//         const centerY = touch.pageY - (dotHeight / 2);
-        
-//         dot.style.top = `${centerY}px`;
-//         dot.style.left = `${centerX}px`;
-//     })
-// })
-
-// //Disables default browser events when moving (NO ZOOM, NO REFRESH ON PULL)
-// document.addEventListener('touchmove', function(event) {
-//     event.preventDefault();
-// }, { passive: false }); // can call preventDefault() without causing any performance issues.
-
-
-// //When letting go of touchscreen
-// document.addEventListener("touchend", e => {
-//     [...e.changedTouches].forEach(touch =>{     //for each touch
-//         const dot = document.getElementById(touch.identifier);
-//         dot.remove();
-        
-//         endPos[0] = touch.pageX;
-//         endPos[1] = touch.pageY;
-        
-//         //console.log(startPos + " startVector");
-//         //console.log(endPos + " endVector");
-        
-//         //Calculate Vector and send as string
-//         var vector = VectorCalculation(startPos, endPos);
-//         vectorString = (-vector[0]) + "," + vector[1];
-//         movementString = ":m:"+vectorString;
-//         //sending vector to server
-//         websocket.send(selfId + movementString);
-//         h2.innerHTML = vectorString;
-//     })
-// })
-
-// //Calculating Vector of touchstart and touchend
-// function VectorCalculation(_startPos, _endPos){
-//     //Vector of startVec , endVec
-//     var Vector = [ , ];
-//     Vector[0] = _endPos[0] - _startPos[0];      //X
-//     Vector[1] = _endPos[1] - _startPos[1];      //Y
-    
-//     return Vector
-// }
-
-// //Calculating Length of vector
-// function VectorLength(_startPos, _endPos){
-//     var Vector = [ , ];
-//     Vector[0] = _endPos[0] - _startPos[0];      //X
-//     Vector[1] = _endPos[1] - _startPos[1];      //Y
-    
-//     //Length Calculator
-//     return Math.sqrt(square(Vector[0]) + square(Vector[1]));
-// }
+} 
