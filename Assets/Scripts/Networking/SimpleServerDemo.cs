ec3709b5772441a00009fad29d990fcc4bcf7d79
Omair <513589@student.saxion.nl>
2023-04-18 13:34:15 +0200

    fihcsjd

--- Assets/Scripts/Networking/SimpleServerDemo.cs
+++ Assets/Scripts/Networking/SimpleServerDemo.cs
@@ -81,6 +81,9 @@ public class SimpleServerDemo : MonoBehaviour
         File.WriteAllText(filePath, newContent);
 
 
+
+
+
         // Create a list of active connections:
 
         PlayerMovement.OnPlayerLostHealth += (PlayerMovement player) =>
